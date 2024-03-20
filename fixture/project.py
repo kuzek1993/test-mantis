2e085c452e73ecba02262e5152571fa3e6da6e78
vladimir1993 <kuzek1993@gmail.com>
2024-03-21 01:20:06 +0300

    тест-удаления проекта

--- fixture/project.py
+++ fixture/project.py
@@ -1,4 +1,4 @@
-
+import time
 class ProjectHelper:
     def __init__(self, app):
         self.app = app
@@ -12,3 +12,14 @@ class ProjectHelper:
         wd.find_element_by_name("name").clear()
         wd.find_element_by_name("name").send_keys(project.name)
         wd.find_element_by_xpath("//input[@value='Add Project']").click()
+
+    def delete_project_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Manage").click()
+        wd.find_element_by_link_text("Manage Projects").click()
+        wd.find_element_by_css_selector("a[href='manage_proj_edit_page.php?project_id=%s" % id).click()
+        time.sleep(3)
+        wd.find_element_by_xpath("//input[@value='Delete Project']").click()
+        time.sleep(3)
+        wd.find_element_by_xpath("//input[@value='Delete Project']").click()
+
